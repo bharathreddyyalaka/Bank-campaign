a10efe6d5b2366957e7d61213fa680c06e14e3ee
Bharath Reddy Yalaka <bharathreddyyalaka@Bharaths-MacBook-Air.local>
2025-02-12 20:02:07 -0500

    testing conflicts

--- src/main/java/com/bank/marketing/campaign_service/service/UserDetailsForAuth.java
+++ src/main/java/com/bank/marketing/campaign_service/service/UserDetailsForAuth.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public class UserDetailsForAuth implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Testing conflicts
         return null;
     }
 }
